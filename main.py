16f8b510ac82719fbc2ab87b55e2e46ae3004788
Shresth Pratap <shresthpratap6@gmail.com>
2023-07-22 00:58:31 +0530

    Added frame limiter to 60FPS and caption.

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pygame as pg
 from sys import exit
 
 screen = pg.display.set_mode((800, 600), 32)
+pg.display.set_caption("Game")
+clock = pg.time.Clock()
 running = True
+
 while running:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             pg.quit()
-            exit();
-    pg.display.flip();
+            exit()
+    pg.display.flip()
+    clock.tick(60)
