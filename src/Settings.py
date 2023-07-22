6793607a1183686b32429d657089d721300b343b
Mr-TM-YT <www.mohamedsalwa13@gmail.com>
2023-07-22 13:33:01 +0300

    Got rid of unused vars in Settings.py

--- src/Settings.py
+++ src/Settings.py
@@ -6,8 +6,4 @@ WINDOW_SIZE = {
 # Temp code
 square_size = 50  # I just felt like adding a comment ;) [Mr.TM]
 square_color = (255, 0, 0)  # square color "red"
-square_x = WINDOW_SIZE["x"] / 2 - square_size / \
-    2  # The square should be centerd [Mr.TM]
-square_y = WINDOW_SIZE["y"] / 2 - square_size / \
-    2  # The square should be centerd [Mr.TM]
 movement_speed = 5  # movement speed of the square
