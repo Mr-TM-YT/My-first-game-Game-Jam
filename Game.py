8398e00b359271047bfd13922bb77dbc21e04c6b
Shresth Pratap <shresthpratap6@gmail.com>
2023-07-22 12:28:45 +0530

    Necessary changes done, code is working now, cleanup done.

--- Game.py
+++ Game.py
@@ -1,70 +1,68 @@
+# necessary imports
 import pygame as pg
 from sys import exit
 from Settings import *
 
+# initialize pygame
 pg.init()
 
-class Game:
-    # Start vars [Mr.TM]
 
+class Game:
+    #  necessary variables
     running = True
     display = None
     pg.display.set_caption("The (Team Name) is here baby")
     clock = pg.time.Clock()
+    keys = pg.key.get_pressed()
 
-    # Start temp [Mr.TM]
-
-    keys = pg.key.get_pressed();
-
-    # End temp [Mr.TM]
-
-    # End vars [Mr.TM]
-
-    # Start methods [Mr.TM]
+    #  init method
     def __init__(self) -> None:
-        self.Init() # Don't ask me why I made a seperate init function
+        self.Init()
+        # Initial x position of the square
+        self.square_x = WINDOW_SIZE["x"] // 2
+        # Initial y position of the square
+        self.square_y = WINDOW_SIZE["y"] // 2
 
-    def IsRunning(self) -> bool: # This will help us do the main loop outside of the Game class [Mr.TM]
-        return self.running;
+    # checks if the game is running
+    def IsRunning(self) -> bool:
+        return self.running
 
-    def Init(self) -> None: # the __init__ is not enough for me so I'll have a seperate init method [Mr.TM]
-        self.display = pg.display.set_mode((WINDOW_SIZE["x"], WINDOW_SIZE["y"]), 32)
+    # idk why another init 🤔
+    def Init(self) -> None:
+        self.display = pg.display.set_mode(
+            (WINDOW_SIZE["x"], WINDOW_SIZE["y"]))
 
-    def Update(self) -> None: # Called 60 times a second [Mr.TM]
-        self.CheckEvents() # We're modular, You know [Mr.TM]
+    # update the game
+    def Update(self) -> None:
+        self.CheckEvents()
 
+    # draw stff on the screen
+    def Render(self) -> None:
+        self.display.fill((0, 0, 255))
+        pg.draw.rect(self.display, square_color, (self.square_x,
+                                                  self.square_y, square_size, square_size))
+        pg.display.flip()
+        self.clock.tick(60)
 
-    def Render(self) -> None: # IDK but I felt adding this comment, Man the code is so concise and nice [Mr.TM]
-        self.display.fill((0, 255, 0))
-
-        # I will not make a seperate function just for drawing the "temp" player [Mr.TM]
-        pg.draw.rect(self.display, square_color, (square_x,
-                    square_y, square_size, square_size))
-        
-        pg.display.flip();
-        self.clock.tick(60) # This should be in the Update but I'll put it in Render [Mr.TM]
-
-    def CheckEvents(self) -> None: # I felt being modular today tbh [Mr.TM]
+    # check for events
+    def CheckEvents(self) -> None:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 self.running = False
 
+        self.keys = pg.key.get_pressed()  # Update keys state on each iteration
+
+        # Now use self.square_x and self.square_y to update the position
+        if self.keys[pg.K_LEFT] or self.keys[pg.K_a]:
+            self.square_x -= movement_speed
+        if self.keys[pg.K_RIGHT] or self.keys[pg.K_d]:
+            self.square_x += movement_speed
+        if self.keys[pg.K_UP] or self.keys[pg.K_w]:
+            self.square_y -= movement_speed
+        if self.keys[pg.K_DOWN] or self.keys[pg.K_s]:
+            self.square_y += movement_speed
+
+    # close the game and cleanup
     def Close(self):
         pg.quit()
         exit()
-    
-    
-    # NOTE: HandleTempPlayerInput(slef) method doesn't work for some reason (I called it from the Update method don't worry) [Mr.TM]
-    # TODO: Fix the HandleTempPlayerInput(self) method
-
-    # def HandleTempPlayerInput(self): # Even if it will get deleted later, I have to make it modular [Mr.TM]
-    #     if self.keys[pg.K_LEFT] or self.keys[pg.K_a]:
-    #         square_x -= movement_speed
-    #     if self.keys[pg.K_RIGHT] or self.keys[pg.K_d]:
-    #         square_x += movement_speed
-    #     if self.keys[pg.K_UP] or self.keys[pg.K_w]:
-    #         square_y -= movement_speed
-    #     if self.keys[pg.K_DOWN] or self.keys[pg.K_s]:
-    #         square_y += movement_speed
-
-    # End methods [Mr.TM]
--- main.py
+++ main.py
@@ -1,61 +1,16 @@
-# # importing necessary stuff
-# import pygame as pg
-# from sys import exit
-
-# # necessary variables
-# screen = pg.display.set_mode((800, 600))
-# pg.display.set_caption("Game")
-# clock = pg.time.Clock()
-# running = True
-# # temp player code
-# square_size = 50
-# square_color = (255, 0, 0)  # square color "red"
-# square_x, square_y = 800 // 2 - square_size, 600 // 2 - \
-#     square_size  # position of the square center of the screen
-# movement_speed = 5  # movement speed of the square
-
-# # game loop
-# # Making a def makes you sexy [Mr.TM]
-
-# while running:
-#     # checking for updates
-#     for e in pg.event.get():
-#         if e.type == pg.QUIT:
-#             pg.quit()
-#             exit()
-
-#     # get the state of the keys (pressed/released)
-#     keys = pg.key.get_pressed()
-
-#     # movement for the player
-#     if keys[pg.K_LEFT] or keys[pg.K_a]:
-#         square_x -= movement_speed
-#     if keys[pg.K_RIGHT] or keys[pg.K_d]:
-#         square_x += movement_speed
-#     if keys[pg.K_UP] or keys[pg.K_w]:
-#         square_y -= movement_speed
-#     if keys[pg.K_DOWN] or keys[pg.K_s]:
-#         square_y += movement_speed
-
-#     # drawing stff goes here
-#     screen.fill((255, 255, 255))
-#     pg.draw.rect(screen, square_color, (square_x,
-#                 square_y, square_size, square_size))
-
-#     # updating the screen and limiting the fps to 60
-#     pg.display.flip()
-#     clock.tick(60)
-
+# necessary imports
 from Game import Game
 
+# create an instance of the game class 
 game = Game()
 
+# main method to run the game
 def main():
     while game.IsRunning():
         game.Update()
         game.Render()
     game.Close()
 
-
+# only execute the main func if this file is being run
 if __name__ == "__main__":
     main()
